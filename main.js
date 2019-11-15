0ab50d2b8a0a7f69a4b9283a54b69eb91cfd6374
Ben Harris <benjaminharris28@gmail.com>
2019-11-14 22:14:22 -0500

    new files

---
+++ main.js
@@ -0,0 +1,11 @@
+
+// Listen for button click on hamburger icon and show nav
+// Create hamburger nav html
+
+$("body").on('click', '.hamburger', function() {
+    $(".nav-links").toggleClass("nav-show");
+});
+
+$("body").on('click', 'li', function() {
+    $(".nav-links").toggleClass("nav-show");
+});
